5f961fbf26caf3fe41769c15b8dd8c4ea2e0adb5
MUSEFacility <facilitymuse@gmail.com>
2025-06-16 19:34:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/checkout', async (req, res) => {
     const infoOwner = await transporter.sendMail({
       from: `"MUSE.holiday Biancheria Garda" <${process.env.SMTP_USER}>`,
       to:   process.env.SHOP_EMAIL,
-      cc:   'info@muse.holiday',
+      cc:   'garda@muse.holiday',
       subject: `Ordine ricevuto: ${name}`,
       html:    summaryHtml
     });
